40afae7015d00b90448256d0faec25d2e59da10c
jdoherty513 <jdoherty513@gmail.com>
2022-06-29 22:15:30 -0400

    Add weather panel

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <UpcomingDeliveries />
+  <div>
+    <UpcomingDeliveries />
+    <TodaysWeather />
+  </div>
 </template>
 
 <script>
 import UpcomingDeliveries from '~/components/UpcomingDeliveries.vue'
+import TodaysWeather from '~/components/TodaysWeather.vue'
+
 export default {
   name: 'IndexPage',
-  components: { UpcomingDeliveries },
+  components: { UpcomingDeliveries, TodaysWeather },
 }
 </script>
