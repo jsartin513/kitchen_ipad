797196c5ebe186e8c4a96cce6c2499e0578f2eac
jdoherty513 <jdoherty513@gmail.com>
2022-07-01 14:57:23 -0400

    apparently i haven't committed this file til now --facepalm--

---
+++ components/TodaysWeather.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title>Current Weather</v-card-title>
+      <v-card-text v-if="showWeather">
+        Temperature: {{ weather.temperature }}
+      </v-card-text>
+      <v-card-text v-if="showFailureMessage"> Weather Load Failed </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import apiTokens from '../tokens.js'
+export default {
+  name: 'TodaysWeather',
+  props: {},
+  data() {
+    return {
+      weather: {},
+      weatherLoaded: false,
+      weatherLoadFailed: false,
+    }
+  },
+  computed: {
+    showWeather() {
+      return this.weatherLoaded && !this.weatherLoadFailed
+    },
+    showFailureMessage() {
+      return this.weatherLoadFailed
+    },
+  },
+  created() {
+    this.loadWeather()
+  },
+  methods: {
+    loadWeather() {
+      const getTimelineURL = 'https://api.tomorrow.io/v4/timelines'
+      const apiKey = apiTokens.tomorrowWeatherToken
+      const location = '42.384481,-71.0880066'
+      const units = 'imperial'
+      const timesteps = 'current'
+      const fields = 'temperature'
+      const getTimeLineParameters = `?apikey=${apiKey}&location=${location}&units=${units}&timesteps=${timesteps}&fields=${fields}`
+      fetch(getTimelineURL + getTimeLineParameters)
+        .then((response) => response.json())
+        .then((response) => {
+          this.weather = response.data.timelines[0].intervals[0].values
+          this.weatherLoaded = true
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error)
+          this.weatherLoadFailed = true
+        })
+    },
+  },
+}
+</script>
