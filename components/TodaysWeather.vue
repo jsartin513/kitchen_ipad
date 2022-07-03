00c1a54b1721a0903a0973ddd677b7d4a566911e
jdoherty513 <jdoherty513@gmail.com>
2022-07-03 00:33:04 -0400

    add some weather icons

--- components/TodaysWeather.vue
+++ components/TodaysWeather.vue
@@ -3,7 +3,24 @@
     <v-card class="w-full h-full" elevation="2">
       <v-card-title>Current Weather</v-card-title>
       <v-card-text v-if="showWeather">
-        <div class="text-6xl">{{ currentTemperature }}&deg;F</div>
+        <div class="grid gap-2 grid-cols-2 px-2">
+          <div>
+            <div class="text-6xl">{{ currentTemperature }}&deg;F</div>
+            <div class="text-base">
+              Feels like {{ Math.trunc(weather.temperatureApparent) }}&deg;F
+            </div>
+          </div>
+          <div>
+            <v-img
+              :max-width="148"
+              :max-height="60"
+              :src="conditionIcon"
+              :contain="true"
+              alt="Weather icon"
+            />
+            <div class="text-base">{{ currentConditions }}</div>
+          </div>
+        </div>
       </v-card-text>
       <v-card-text v-if="showFailureMessage"> Weather Load Failed </v-card-text>
     </v-card>
@@ -32,6 +49,54 @@ export default {
     showFailureMessage() {
       return this.weatherLoadFailed
     },
+    conditionIcon() {
+      if (this.weather) {
+        switch (this.weather.weatherCode) {
+          case 1000:
+            return 'sun.png'
+          case 1101:
+          case 1100:
+            return 'partly_cloudy.png'
+          case 1102:
+          case 2100:
+          case 2000:
+            return 'cloud.png'
+          case 4000:
+          case 4200:
+          case 4001:
+          case 4201:
+            return 'raindrop.pngn'
+        }
+      }
+      return null
+    },
+    currentConditions() {
+      if (this.weather) {
+        switch (this.weather.weatherCode) {
+          case 1000:
+            return 'Clear'
+          case 1101:
+            return 'Partly Cloudy'
+          case 1100:
+            return 'Mostly Clear'
+          case 1102:
+            return 'Mostly Cloudy'
+          case 2100:
+            return 'Light Fog'
+          case 2000:
+            return 'Fog'
+          case 4000:
+            return 'Drizzle'
+          case 4200:
+            return 'Light Rain'
+          case 4001:
+            return 'Rain'
+          case 4201:
+            return 'Heavy Rain'
+        }
+      }
+      return null
+    },
   },
   created() {
     this.loadWeather()
@@ -43,7 +108,7 @@ export default {
       const location = '42.3928182,-71.099294' // TODO: get from browser location
       const units = 'imperial'
       const timesteps = 'current'
-      const fields = 'temperature'
+      const fields = ['temperature', 'temperatureApparent', 'weatherCode']
       const getTimeLineParameters = `?apikey=${apiKey}&location=${location}&units=${units}&timesteps=${timesteps}&fields=${fields}`
       fetch(getTimelineURL + getTimeLineParameters)
         .then((response) => response.json())
@@ -54,8 +119,7 @@ export default {
         .catch((error) => {
           // eslint-disable-next-line no-console
           console.log(error)
-          // TODO: disable this once I can hit the API againn
-          this.weatherLoaded = true
+          this.weatherLoadFailed = true
         })
     },
   },
