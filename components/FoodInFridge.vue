08fe7116f2db63286b620479cdb8fedd8842f6f9
jdoherty513 <jdoherty513@gmail.com>
2022-07-08 17:32:19 -0400

    first pass at styles

--- components/FoodInFridge.vue
+++ components/FoodInFridge.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-card class="w-full h-full">
-      <v-card-title>Make effort to eat...</v-card-title>
+    <v-card class="w-full h-full" elevation="2" color="rgb(199 210 254)">
+      <v-card-title class="text-indigo-800">Make effort to eat...</v-card-title>
       <v-card-text class="mx-4"
         ><ul class="list-disc">
           <li>Fresh fruit (pears! apples! peaches!)</li>
