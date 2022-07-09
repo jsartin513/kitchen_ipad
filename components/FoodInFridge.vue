637da97ea5ce262df9ff2a0f7d705aff6a503ac6
jdoherty513 <jdoherty513@gmail.com>
2022-07-08 22:57:15 -0400

    fix colors and add some more styles

--- components/FoodInFridge.vue
+++ components/FoodInFridge.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <v-card class="w-full h-full" elevation="2" color="rgb(199 210 254)">
-      <v-card-title class="text-indigo-800">Make effort to eat...</v-card-title>
+    <v-card class="w-full h-full" elevation="2">
+      <v-card-title class="text-emerald-800"
+        >Make effort to eat...</v-card-title
+      >
       <v-card-text class="mx-4"
         ><ul class="list-disc">
           <li>Fresh fruit (pears! apples! peaches!)</li>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="bg-slate-300">
-    <h1 class="px-16 py-8 text-3xl text-center text-indigo-700">
-      What's going on?
-    </h1>
-    <div class="grid gap-4 grid-cols-2 px-8">
-      <UpcomingDeliveries />
-      <TodaysWeather />
-      <FoodInFridge />
-      <FunInfo />
-      <NextSoxGame />
+  <v-main>
+    <div class="bg-indigo-200 h-full">
+      <h1 class="px-16 py-8 text-3xl text-center text-emerald-800">
+        What's going on?
+      </h1>
+      <div class="grid gap-4 grid-cols-2 px-8">
+        <UpcomingDeliveries />
+        <TodaysWeather />
+        <FoodInFridge />
+        <FunInfo />
+        <NextSoxGame />
+      </div>
     </div>
-  </div>
+  </v-main>
 </template>
 
 <script>
