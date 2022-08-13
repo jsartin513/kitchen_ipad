c0fa31a06b40063e73cefa2e47f6355ceb46773c
jdoherty513 <jdoherty513@gmail.com>
2022-08-13 14:56:56 -0400

    fix http

--- components/3DPrinter.vue
+++ components/3DPrinter.vue
@@ -4,7 +4,7 @@
       <v-card-title class="text-emerald-800">3D Printer</v-card-title>
       <v-card-text> Print integration not yet implemented... </v-card-text>
       <v-card-actions
-        ><a href="https://maker.local/" target="_blank"
+        ><a href="http://maker.local/" target="_blank"
           ><v-btn class="mx-4">Open print monitor</v-btn></a
         ></v-card-actions
       ></v-card
